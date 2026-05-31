29bfb0ae3e34ae2cd16a455d56e3c5df8d9b211d
Daniel Cruz <vulpine444@gmail.com>
2026-05-31 22:53:48 +0000

    set next-sitemap outDir to public

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   siteUrl: 'https://vulpinehomes.com',
   generateRobotsTxt: true,
+  outDir: './public',
   changefreq: 'weekly',
   priority: 0.7,
   sitemapSize: 5000,
   robotsTxtOptions: {
     policies: [{ userAgent: '*', allow: '/' }],
-    additionalSitemaps: [],
   },
 };
