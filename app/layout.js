05033e76f3dc184fb0098a68e9dd031302a25e44
Daniel Cruz <vulpine444@gmail.com>
2026-08-02 13:18:53 +0000

    Set fox emoji 🦊 as global favicon

--- app/layout.js
+++ app/layout.js
@@ -64,14 +64,8 @@ export const metadata = {
   },
   manifest: '/site.webmanifest',
   icons: {
-    icon: [
-      { url: '/favicon.svg', type: 'image/svg+xml' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
+    icon: '/favicon.svg',
+    apple: '/favicon.svg',
   },
 };
 
